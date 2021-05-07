208a18cc7155f83cacd5d98e54b3948415217a14
xwi88 <278810732@qq.com>
2021-05-07 18:58:01 +0800

    add gitlab (#1)
    * add gitlab
    
    * add gitlab-ce

---
+++ gitlab/gitlab-ce/nightly/docker-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=nightly
+USER=v8fg
+NAME=gitlab-ce
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
