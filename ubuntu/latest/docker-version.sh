556a4b60c6d19e68bc1afb4fc15a69c2d19ec057
xwi88 <278810732@qq.com>
2021-05-26 14:28:46 +0800

    add ubuntu image (#6)
    * fixed README (#5)
    
    * add ubuntu image

---
+++ ubuntu/latest/docker-version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=latest
+USER=v8fg
+NAME=ubuntu
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+# DAEMON="-d"
