43c100ddf200b7cc37ef7890324f295d3de89d28
xwi88 <278810732@qq.com>
2021-09-02 12:20:41 +0800

    add alpine3.14 with mtr and python3.7.11 image (#15)

---
+++ alpine/3.14-kit/docker-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=3.14-kit
+USER=v8fg
+NAME=alpine
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
