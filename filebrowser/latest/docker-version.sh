01bf573a4547ddf4e0044fe4dba4f68042e6029b
xwi88 <278810732@qq.com>
2021-12-23 18:26:57 +0800

    add filebrowser

---
+++ filebrowser/latest/docker-version.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=latest
+USER=v8fg
+NAME=filebrowser
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+
+PORT="-p 18888:80"
