a1cb2d6e39928f94d3ca89cff5335eb93d0b0257
xwi88 <278810732@qq.com>
2022-01-19 12:15:28 +0800

    add php

---
+++ php/latest/docker-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=latest
+USER=v8fg
+NAME=php
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+
+ENV="-it --rm "
+PARAMS=" /bin/bash"
