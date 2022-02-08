186a56d68d1a19883120e29d91893612ad0f84e9
xwi88 <278810732@qq.com>
2022-02-08 14:12:40 +0800

    add nats

---
+++ nats/2.7-linux/docker-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=2.7-linux
+USER=v8fg
+NAME=nats
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+
+ENV="-v $PWD/conf/:/etc/nats/"
+PORT=" -p 4222:4222 -p 6222:6222 -p 8222:8222 "
