07411a787c4477f594b23e9ae7e18ba8be966a30
xwi88 <278810732@qq.com>
2022-02-11 15:34:09 +0800

    upgrade elk and postgres

---
+++ logstash/7.17.0/docker-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=7.17.0
+USER=v8fg
+NAME=logstash
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
