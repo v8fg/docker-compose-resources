84bf5cd440a5704796963e9b33fd6eadb0e8ca69
xwi88 <278810732@qq.com>
2022-11-03 18:44:44 +0800

    upgrade go1.18.8 and go1.19.3

---
+++ golang/official-1.18.8-alpine3.13/docker-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=official-1.18.8-alpine3.13
+USER=v8fg
+NAME=golang
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
