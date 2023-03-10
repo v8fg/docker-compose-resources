ce26d5d21bff66e0e4de4c88de563e989d4bd676
xwi88 <278810732@qq.com>
2023-03-10 15:32:47 +0800

    upgrade go1.19.6 and go1.20.1 (#75)

---
+++ ubuntu/18.04-go1.20.1-tf-cpu/docker-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=18.04-go1.20.1-tf-cpu
+USER=v8fg
+NAME=ubuntu
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
