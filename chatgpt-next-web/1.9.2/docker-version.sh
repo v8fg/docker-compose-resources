42ad2af194c5d283c1e9a424d41351859f672ddb
xwi88 <278810732@qq.com>
2023-04-16 11:16:21 +0800

    chatgpt-next-web (#78)

---
+++ chatgpt-next-web/1.9.2/docker-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=1.9.2
+USER=v8fg
+NAME=chatgpt-next-web
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+
+# ENV="-e OPENAI_API_KEY= -e CODE="
+PORT="-p 3690:3000"
