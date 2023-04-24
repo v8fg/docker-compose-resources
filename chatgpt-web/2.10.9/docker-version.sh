8d64438b987eb41468f6d318bfad6620c46b7a95
xwi88 <278810732@qq.com>
2023-04-24 11:41:56 +0800

    chatgpt-web (#80)

---
+++ chatgpt-web/2.10.9/docker-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=2.10.9
+USER=v8fg
+NAME=chatgpt-web
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+
+ENV="-e OPENAI_API_KEY= -e AUTH_SECRET_KEY="
+PORT="-p 3630:3002"
