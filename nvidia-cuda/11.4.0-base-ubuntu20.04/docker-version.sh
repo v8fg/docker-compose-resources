f0c17675a72bf6b55a007476d665f46de047fa5d
xwi88 <278810732@qq.com>
2023-05-05 10:58:37 +0800

    stable diffusion webui (#82)

---
+++ nvidia-cuda/11.4.0-base-ubuntu20.04/docker-version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=11.4.0-ubuntu20.04
+USER=v8fg
+NAME=nvidia-cuda-base
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
+
+# ENV="-e "
+# PORT="-p "
