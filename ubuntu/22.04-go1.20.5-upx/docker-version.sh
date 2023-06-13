e38968f63299603543fc9f7fc32423529ba0d580
xwi88 <278810732@qq.com>
2023-06-13 16:05:53 +0800

    upgrade go1.19.10 and go1.20.5 (#87)

---
+++ ubuntu/22.04-go1.20.5-upx/docker-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# image info, version may auto update
+TAG=22.04-go1.20.5-upx
+USER=v8fg
+NAME=ubuntu
+
+REPOSITORY=${USER}/${NAME}
+# image with tag, use to push image
+LOCATION=${REPOSITORY}:${TAG}
+
+# use to build container
+CONTAINER_NAME=${USER}-${NAME}-${TAG}
